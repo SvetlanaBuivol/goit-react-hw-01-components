bcea5336440d46aff9423257ec0b6fdea816df01
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-06-30 02:21:27 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import UserProfile from 'components/Profile/UserProfile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export default function App() {
   return (
@@ -18,7 +20,8 @@ export default function App() {
           likes={user.stats.likes}
       />
       <Statistics title='Upload stats' stats={data} />
-      <FriendList friends={friends}/>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
       </div>
   );
 }
